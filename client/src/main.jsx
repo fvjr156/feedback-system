5b0f0d94d551afd24a2594cf3c3b16c59810c905
Promatues <114233003+Promatues@users.noreply.github.com>
2024-05-30 18:39:13 +0800

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,6 +11,7 @@ import NotFound from "./pages/NotFoundPage/NotFound";
 import AppToolbar from "./components/NavigationComponents/Toolbar";
 import WarningMessage from "./components/WarningMessage/WarningMessage";
 import Forms from "./pages/FormsPage/Forms";
+import FormsInfos from "./pages/FormsInfoPage/FormsInfos";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/forms" element={<Forms />} />
+          <Route path="/formsInfos" element={<FormsInfos />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
