493a8c6041bbe38d9a8368ffcd1a4f9ee56aa66e
fvjr156 <juniorvillanueva156@gmail.com>
2024-06-02 18:53:21 +0800

    mysql config for coolify database

--- server/routes/routes.mjs
+++ server/routes/routes.mjs
@@ -8,9 +8,15 @@ import {
   GET_Test,
   POST_SubmitForm,
 } from "../controllers/server_controller.mjs";
-import { GET_FormsPageStats, GET_ResponsesPageStats } from "../controllers/data_visualization_operations.mjs";
+import {
+  GET_FormsPageStats,
+  GET_ResponsesPageStats,
+} from "../controllers/data_visualization_operations.mjs";
 const router = Router();
 
+router.get("/healthcheck", function (req, res) {
+  res.status(200).send("OK");
+});
 router.get("", GET_Test);
 router.get("/", GET_Test);
 router.get("/forms", GET_Forms);
@@ -20,11 +26,10 @@ router.get("/responsebyid", GET_ResponseByID);
 router.post("/submit-form", POST_SubmitForm);
 router.get("/stats/forms", GET_FormsPageStats);
 router.get("/stats/response", GET_ResponsesPageStats);
-router.get("/survey", GET_Survey)
+router.get("/survey", GET_Survey);
 
-router.get("*", function(req, res){
+router.get("*", function (req, res) {
   res.status(404).send("Not found");
-})
-
+});
 
 export default router;
