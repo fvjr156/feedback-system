f90aa3f7fe831d083d11629f231fe0ea54930586
fvjr156 <juniorvillanueva156@gmail.com>
2024-06-04 22:28:53 +0800

    v1.2 - Added CSV export, optimized sorting and database functions.

--- server/controllers/server_controller.mjs
+++ server/controllers/server_controller.mjs
@@ -8,17 +8,33 @@ import {
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-
 export const GET_Survey = (req, res) => {
-  const url =
-    "https://forms.office.com/Pages/ResponsePage.aspx?id=Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUMkdMWDRQSkRCTjMxSDFRNVpXVUs4SlpSTS4u"
-  res.redirect(url)
-}
+  const url = [
+    "https://forms.office.com/Pages/ResponsePage.aspx?id=Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUMkdMWDRQSkRCTjMxSDFRNVpXVUs4SlpSTS4u",
+    "https://forms.office.com/Pages/ResponsePage.aspx?id=Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUMFJDUE5XOVAzQ1Y1OEtDWjMwMUVTTkNJQy4u",
+    "https://forms.office.com/Pages/ResponsePage.aspx?id=Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUQVYwTUtDUTdOSVVWUk1OSVdKT1lYVU80Ry4u",
+  ];
+
+  const survey = req.query.id ? req.query.id : null;
+
+  switch (survey) {
+    case "socialissue":
+      res.redirect(url[1]);
+      break;
+    case "aoki":
+      res.redirect(url[2]);
+      break;
+    default:
+      res.redirect(url[0]);
+      return;
+  }
+  return;
+};
 
 export const GET_Test = (req, res) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  res.status(200).sendFile(path.join(__dirname, "..","test.html"));
+  res.status(200).sendFile(path.join(__dirname, "..", "test.html"));
 };
 
 export const GET_Forms = async (req, res) => {
@@ -50,7 +66,8 @@ export const GET_FormByID = async (req, res) => {
       res.status(400).json({
         message: "No msforms_form_id specified, nothing to show.",
         correct_syntax: "/formbyid?id=msforms_form_id",
-        try_this_form_id: "Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUMkdMWDRQSkRCTjMxSDFRNVpXVUs4SlpSTS4u",
+        try_this_form_id:
+          "Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUMkdMWDRQSkRCTjMxSDFRNVpXVUs4SlpSTS4u",
       });
       return;
     }
@@ -71,7 +88,8 @@ export const GET_ResponseByID = async (req, res) => {
       res.status(400).json({
         message: "No msforms_form_id specified, nothing to show.",
         correct_syntax: "/responsebyid?id=msforms_form_id",
-        try_this_form_id: "Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUMkdMWDRQSkRCTjMxSDFRNVpXVUs4SlpSTS4u",
+        try_this_form_id:
+          "Q97QyABtNEWjxl_CIYWrmwHypepq4k1ElL_6lzc9HCZUMkdMWDRQSkRCTjMxSDFRNVpXVUs4SlpSTS4u",
       });
       return;
     }
@@ -89,9 +107,10 @@ export const POST_SubmitForm = async (req, res) => {
   try {
     DB_AddSubmittedForm(req.body[0]);
 
-    res.status(200).json({ message: "Form data received and processed successfully" });
+    res
+      .status(200)
+      .json({ message: "Form data received and processed successfully" });
   } catch (error) {
     res.status(400).json({ error: "Failed to process form data" });
   }
 };
-
